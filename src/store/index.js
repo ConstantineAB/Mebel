e7dbc7d9dea1c39ee60ae69d1cfe31eeb07ed79e
ConstantineAB <94063423+ConstantineAB@users.noreply.github.com>
2023-01-02 11:26:29 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gamesReducer from './games/reducer';
 import cartReducer from './cart/reducer';
-
 export const store = configureStore({
     reducer: {
         games: gamesReducer,
         cart: cartReducer
     }
-  })
+})
